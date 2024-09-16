6c44573e9e0056139de5a1879ca59e12f7f2b139
Jim Gay <jim@saturnflyer.com>
2024-09-16 11:02:33 -0400

    Use warn instead of raise to allow installation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,4 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [0.2.1] - Unreleased
 
+### Changed
+
+- Use `warn` instead of `raise` in the Railtie for the slack token warning.
+
 ## [0.2.0] - 2024-09-15
--- lib/discharger/railtie.rb
+++ lib/discharger/railtie.rb
@@ -2,7 +2,7 @@ module Discharger
   class Railtie < ::Rails::Railtie
     config.after_initialize do |app|
       if Rails.env.development? && Discharger.slack_token.nil?
-        raise "Your application Discharger.slack_token must be set in the environment"
+        warn "Your application Discharger.slack_token must be set."
       end
     end
   end
