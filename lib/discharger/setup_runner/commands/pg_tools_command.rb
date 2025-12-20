6a2d3694063a9f898e544f9e78f09ca3e215dc5c
ryan.weiss <rymiwe@gmail.com>
2025-12-19 16:37:04 -0800

    Fix PgToolsCommand to use correct config accessor
    The Configuration class uses `db_config` for database settings, not
    `database`. This fixes the command to use the correct accessor when
    checking can_execute? and reading the container name.

--- lib/discharger/setup_runner/commands/pg_tools_command.rb
+++ lib/discharger/setup_runner/commands/pg_tools_command.rb
@@ -13,7 +13,7 @@ module Discharger
         end
 
         def can_execute?
-          config.respond_to?(:database) && config.database&.name.present?
+          config.respond_to?(:db_config) && config.db_config&.name.present?
         end
 
         def execute
@@ -25,7 +25,7 @@ module Discharger
         private
 
         def container_name
-          config.database.name
+          config.db_config.name
         end
 
         def create_pg_tools_directory
